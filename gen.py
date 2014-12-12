cb4cf26c96c95498a0a2a142202a4e8692fe3c7e
arm19jong <s7070132@kmitl.ac.th>
2014-12-12 23:33:41 +0700

    test input module and fix bug

--- gen.py
+++ gen.py
@@ -7,7 +7,7 @@
 วัน(list)
 ชื่อวิชา+ตึก+ห้อง(list)
 '''
-from datetime import date
+#from datetime import date
 from xlwt import *
 import random
 list_color= [0x21, 0x0B, 0x35, 0x11, 0x016, 0x012, 0x0D, 0x10, 0x17, 0x0C, 0x0F]
@@ -21,11 +21,82 @@ list_name = ['MATHEMATICS FOR INFORMATION TECHNOLOGY'
     , 'COMPUTER PROGRAMMING', 'COMPUTER SYSTEMS ORGANIZATION AND OPERATING SYSTEM'
     , 'MULTIMEDIA AND WEB TECHNOLOGY', 'THAI GEOSOCIAL DESIGN'
     , 'FOUNDATION ENGLISH 2', 'COMPUTER PROGRAMMING[Lab]', 'MULTIMEDIA AND WEB TECHNOLOGY[Lab]']
+
+
+6
+['09:00-12:00', '13:00-15:00', '13:00-16:00', '09:00-11:00', '15:00-18:00', '09:00-12:00']
+['Thu', 'Thu', 'Tue', 'Wed', 'Mon', 'Mon']
+['MATHEMATICS FOR INFORMATION TECHNOLOGY[3]', 'COMPUTER PROGRAMMING[3]', 'COMPUTER SYSTEMS ORGANIZATION AND OPERATING SYSTEM[3]', 'MULTIMEDIA AND WEB TECHNOLOGY[3]', 'THAI GEOSOCIAL DESIGN[3]', 'FOUNDATION ENGLISH 2[3]']
 '''
 num = input()
-list_time = raw_input()
-list_day = raw_input()
-list_name = raw_input()
+list_time = input()
+list_day = input()
+list_name = input()
+
+for i in xrange(len(list_name)):
+    print list_name[i]
+    print type(list_name[i])
+    if list_name[i].count('INFORMATION TECHNOLOGY FUNDAMENTALS')>=1:
+        list_name.append('INFORMATION TECHNOLOGY FUNDAMENTALS')
+        list_day.append('Thu')
+        num += 1
+        if list_name[i].count('[1]')>=1:
+            list_time.append('09:00-11:00')
+        elif list_name[i].count('[2]')>=1:
+            list_time.append('12:00-14:00')
+        else:
+            list_time.append('14:00-16:00')
+        list_name[i]=list_name[i][:len(list_name[i])-4]+'[Lab]'
+    elif list_name[i].count('PROBLEM SOLVING IN INFORMATION TECHNOLOGY')>=1:
+        list_name.append('PROBLEM SOLVING IN INFORMATION TECHNOLOGY')
+        list_day.append('Thu')
+        num += 1
+        if list_name[i].count('[1]')>=1:
+            list_time.append('14:00-16:00')
+        elif list_name[i].count('[2]')>=1:
+            list_time.append('09:00-11:00')
+        else:
+            list_time.append('12:00-14:00')
+        list_name[i]=list_name[i][:len(list_name[i])-4]+'[Lab]'
+    elif list_name[i].count('INTRODUCTION TO COMPUTER SYSTEMS')>=1:
+        list_name.append('INTRODUCTION TO COMPUTER SYSTEMS')
+        list_day.append('Thu')
+        num += 1
+        if list_name[i].count('[1]')>=1:
+            list_time.append('12:00-14:00')
+        elif list_name[i].count('[2]')>=1:
+            list_time.append('14:00-16:00')
+        else:
+            list_time.append('09:00-11:00')
+        list_name[i]=list_name[i][:len(list_name[i])-4]+'[Lab]'
+    elif list_name[i].count('COMPUTER PROGRAMMING')>=1:
+        list_name.append('COMPUTER PROGRAMMING')
+        list_day.append('Fri')
+        num += 1
+        if list_name[i].count('[1]')>=1:
+            list_time.append('09:00-11:00')
+        elif list_name[i].count('[2]')>=1:
+            list_time.append('12:00-14:00')
+        else:
+            list_time.append('14:00-16:00')
+        list_name[i]=list_name[i][:len(list_name[i])-4]+'[Lab]'
+    elif list_name[i].count('MULTIMEDIA AND WEB TECHNOLOGY')>=1:
+        list_name.append('MULTIMEDIA AND WEB TECHNOLOGY')
+        list_day.append('Fri')
+        num += 1
+        if list_name[i].count('[1]')>=1:
+            list_time.append('12:00-14:00')
+        elif list_name[i].count('[2]')>=1:
+            list_time.append('14:00-16:00')
+        else:
+            list_time.append('09:00-11:00')
+        list_name[i]=list_name[i][:len(list_name[i])-4]+'[Lab]'
+    else:
+        list_name[i]=list_name[i][:len(list_name[i])-4]
+print num
+print list_time
+print list_day
+print list_name
 
 main_day = ['0', '1', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri']
 main_time = ['0', '08:00', '08:30', '09:00', '09.30', '10.00'
@@ -33,23 +104,6 @@ main_time = ['0', '08:00', '08:30', '09:00', '09.30', '10.00'
             , '13:00', '13:30', '14:00', '14:30', '15:00'
             , '15:30', '16:00', '16:30', '17:00', '17:30'
             , '18:00', '18:30', '19:00', '19:30', '20:00']
-'''
-borders = Borders()
-borders.left = Borders.THICK
-borders.right = Borders.THICK
-borders.top = Borders.THICK
-borders.bottom = Borders.THICK
-
-pattern = Pattern()
-pattern.pattern = Pattern.SOLID_PATTERN
-pattern.pattern_fore_colour = 0x21
-
-style = XFStyle()
-style.num_format_str='YYYY-MM-DD'
-style.font = fnt
-style.borders = borders
-style.pattern = pattern
-'''
 def main_table(sheet):
     borders = Borders()
     borders.left = Borders.THICK
@@ -83,9 +137,6 @@ def main_table(sheet):
     style.alignment.wrap = 1
 
     print sheet.col(0)
-
-    #sheet.write_merge(0, 0, 0, 1, 'Long Cell', style)
-
     sheet.write(1,0,'', style)
     sheet.write(2,0,'Mon', style)
     sheet.write(3,0,'Tue', style)
@@ -105,15 +156,6 @@ def main_table(sheet):
     sheet.write_merge(1,1,19,20,'17.00-18.00', style)
     sheet.write_merge(1,1,21,22,'18.00-19.00', style)
     sheet.write_merge(1,1,23,24,'19.00-20.00', style)
-    '''
-    pattern2 = Pattern()
-    pattern2.pattern = Pattern.SOLID_PATTERN
-    pattern2.pattern_fore_colour = random.choice(list_color)
-    style2 = XFStyle()
-    style2.pattern = pattern2
-    style2.alignment = alignment
-    sheet.write_merge(2,2,1,4,'BUSINESS FUNDAMENTALS FOR INFORMATION TECHNOLOGY \n (ไอที M23)', style2)
-    '''
     list_test=[]
     for i in xrange(num):
         pattern2 = Pattern()
@@ -123,6 +165,7 @@ def main_table(sheet):
         style2.pattern = pattern2
         style2.alignment = alignment
         style2.borders = borders
+        #print 1
         sheet.write_merge(main_day.index(list_day[i]),main_day.index(list_day[i]),main_time.index(list_time[i][:5]),main_time.index(list_time[i][6:])-1,list_name[i], style2)
         list_test.append([main_day.index(list_day[i]),main_day.index(list_day[i]),main_time.index(list_time[i][:5]),main_time.index(list_time[i][6:])-1])
     print sorted(list_test)
@@ -160,9 +203,8 @@ def main_table(sheet):
                     for m in xrange(cou, 24, 2):
                         sheet.write_merge(j,j,m,m+1,'', style3)
                     break
-            #print list_test[i]
 
-    
+
 book = Workbook(encoding='UTF-8')
 sheet = book.add_sheet('A Date')
 
